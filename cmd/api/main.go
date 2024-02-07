80fb63f690ad9aab762c6f8e69cf05525ba46e3f
John Fercher <johnathanfercher22@gmail.com>
2024-02-06 23:33:34 -0300

    keep

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"github.com/johnfercher/go-struct/pkg/domain/entities"
+	"github.com/johnfercher/go-struct/pkg/domain/filesystem"
 	"github.com/johnfercher/go-struct/pkg/services"
 	"log"
 )
@@ -13,7 +16,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, entity := range entities {
-		entity.Print("")
+	filtereds := filterEntrypoints(entities)
+
+	for _, filtered := range filtereds {
+		fmt.Println(filtered.String())
 	}
+}
+
+func filterEntrypoints(entityMap map[string]filesystem.Entity) []*entities.Struct {
+	var filtered []*entities.Struct
+	for _, entity := range entityMap {
+		for _, stru := range entity.Structs {
+			for _, method := range stru.Methods {
+				if method.IsEntrypoint {
+					filtered = append(filtered, stru)
+					break
+				}
+			}
+		}
+	}
+
+	return filtered
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/awalterschulze/gographviz v0.0.0-20200901124122-0eecad45bd71 // indirect
 	github.com/blushft/go-diagrams v0.0.0-20201006005127-c78c821223d9 // indirect
 	github.com/google/uuid v1.1.2 // indirect
-	github.com/johnfercher/go-struct v0.0.2 // indirect
+	github.com/johnfercher/go-struct v0.0.3 // indirect
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 )
